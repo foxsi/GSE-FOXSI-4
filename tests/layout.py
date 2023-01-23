96667757b8a00ecf906f6d80f277f49b3a964ba9
thanasipantazides <panta013@umn.edu>
2023-01-23 14:49:55 -0600

    adding padding to DetectorPanels

--- tests/layout.py
+++ tests/layout.py
@@ -1,9 +1,10 @@
 import sys
 from PyQt6.QtWidgets import QApplication
-# from FoGSE import application
-from FoGSE.application import GSEMain
+# from FOGSE import application
+from FOGSE.application import GSEMain, GSEFocus
 
 app = QApplication([])
 window = GSEMain()
+# window = GSEFocus()
 window.show()
 sys.exit(app.exec())
