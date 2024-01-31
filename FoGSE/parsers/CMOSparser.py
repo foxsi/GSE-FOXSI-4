fixing CMOS plotting issue from scaling the image

FoGSE/parsers/CMOSparser.py
@@ -169,13 +169,13 @@ def QLimageData(data481kB):
     min_value, max_value = 0, 4095
     # QLimage = Image.new("L", (width, height))
     # Set pixel values
-    pixels = [(float(value)-min_value)/(max_value - min_value)*256 for value in decimal_list]
+    # pixels = [(float(value)-min_value)/(max_value - min_value)*256 for value in decimal_list]
     # QLimage.putdata(pixels)
 
     #**********************************************************************
     #******************************** Kris ********************************
     pixels = np.array([(float(value)-min_value)/(max_value - min_value) for value in decimal_list])
-    # pixels[pixels>np.quantile(pixels, 0.9)] = np.quantile(pixels, 0.9)
+    # pixels[pixels>np.quantile(pixels, 0.99)] = np.quantile(pixels, 0.99)
     # pixels[pixels<np.quantile(pixels, 0.1)] = np.quantile(pixels, 0.1)
     # pixels = (pixels-np.min(pixels))/(np.max(pixels-np.min(pixels)))*256
     #**********************************************************************
@@ -238,14 +238,11 @@ def PCimageData(data577kB,frame_no=0):
     min_value, max_value = 0, 4095
     # PCimage = Image.new("L", (width, height))
     # Set pixel values
-    pixels = [(float(value)-min_value)/(max_value - min_value)*256 for value in decimal_list]
+    # pixels = [(float(value)-min_value)/(max_value - min_value)*256 for value in decimal_list]
 
     #**********************************************************************
     #******************************** Kris ********************************
     pixels = np.array([(float(value)-min_value)/(max_value - min_value) for value in decimal_list])
-    pixels[pixels>np.quantile(pixels, 0.9)] = np.quantile(pixels, 0.9)
-    pixels[pixels<np.quantile(pixels, 0.1)] = np.quantile(pixels, 0.1)
-    pixels = (pixels-np.min(pixels))/(np.max(pixels-np.min(pixels)))*256
     #**********************************************************************
 
     # PCimage.putdata(pixels)

FoGSE/windows/CMOSQLWindow.py
@@ -106,7 +106,7 @@ class CMOSQLWindow(QWidget):
         # send image to frame and add to plot
         self.img = QtWidgets.QGraphicsPixmapItem(pg.QtGui.QPixmap(q_image))
         self.graphPane.addItem(self.img)
-        self.set_image_colour("red")
+        self.set_image_colour("green")
 
     def update_rotation(self, image_angle):
         """ Allow the image rotation to be updated whenever. """
@@ -160,7 +160,7 @@ class CMOSQLWindow(QWidget):
         if self.image_product=="image":
             new_frame = self.reader.collection.image_array()
             new_frame = rotatation.rotate_matrix(matrix=new_frame, angle=self.image_angle)
-            new_frame[new_frame<1e-1] = 0 # because interp 0s causes tiny artifacts
+            new_frame[new_frame<1e-10] = 0 # because interp 0s causes tiny artifacts
             self.update_method = "replace"
 
         # update current plotted data with new frame
@@ -277,7 +277,7 @@ class CMOSQLWindow(QWidget):
         norm = np.max(self.my_array, axis=(0,1))
         norm[norm==0] = 1 # can't divide by 0
         uf = self.max_val*self.my_array//norm
-
+        print(uf)
         # allow this all to be looked at if need be
         self.qImageDetails = [uf.astype(self.numpy_format), self.detw, self.deth, self.cformat]
 