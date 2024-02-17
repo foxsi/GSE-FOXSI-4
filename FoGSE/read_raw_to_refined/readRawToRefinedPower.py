340fbe755d6291e95e25c53480c894468be04f31
Kristopher Cooper <kris@Kristophers-MacBook-Pro.local>
2024-02-17 14:28:55 -0700

    typo

--- FoGSE/read_raw_to_refined/readRawToRefinedPower.py
+++ FoGSE/read_raw_to_refined/readRawToRefinedPower.py
@@ -88,7 +88,7 @@ class PowerReader(ReaderBase):
             print("No data from Powerparser.")
             output, error_flag = ({'unixtime':None, 
                                    0:None, 1:None, 2:None, 3:None, 4:None, 5:None, 6:None, 7:None, 
-                                   8:None, 8:None, 10:None, 11:None, 12:None, 13:None, 14:None, 15:None},
+                                   8:None, 9:None, 10:None, 11:None, 12:None, 13:None, 14:None, 15:None},
                                    None)
         return output, error_flag
 
