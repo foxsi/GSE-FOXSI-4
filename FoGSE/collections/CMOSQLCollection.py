7f9012d7dba2f766623ee82b779c4dfb626188e5
Kristopher Cooper <kris@Kristophers-MacBook-Pro.local>
2024-04-01 13:55:35 -0800

    An edit made code introduced in commit f5cd6cf. Instead of keeping a file containing a CMOS QL mask in it, the function that loads the mask now creates the mask directly. The file is therefore deleted from the repo.

--- FoGSE/collections/CMOSQLCollection.py
+++ FoGSE/collections/CMOSQLCollection.py
@@ -145,15 +145,13 @@ def get_cmos2_ql_ave_background():
 CMOS1_QL_AVE_BACKGROUND = get_cmos1_ql_ave_background()
 CMOS2_QL_AVE_BACKGROUND = get_cmos2_ql_ave_background()
 
-
-MASK_DIR = FILE_DIR+"/../data/cmos_ql_mask/"
-
 def get_cmos_ql_mask():
     """ 
     Return the QL frame mask for both CMOS  QL images to mask the bright 
     feature found in hotter CMOS QL images. 
     """
-    cmos_ql_mask_arr = np.load(MASK_DIR+"cmos_ql_mask.npy")
+    cmos_ql_mask_arr = np.ones((480, 512))
+    cmos_ql_mask_arr[0:128,0:50] = 0
     return cmos_ql_mask_arr
 
 CMOS_QL_MASK_ARRAY = get_cmos_ql_mask()
